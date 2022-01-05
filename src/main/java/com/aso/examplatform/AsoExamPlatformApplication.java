5727f9d86874899e95334e830122b70feb6c5482
Dilina Madhushan <dilinamadhushan.and@gmail.com>
2022-01-05 17:25:49 +0530

    initial commit

---
+++ src/main/java/com/aso/examplatform/AsoExamPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aso.examplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsoExamPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsoExamPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aso/examplatform/AsoExamPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aso.examplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsoExamPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
